5eb1efb2b331e135273270dd272c0a28a66c4af7
bootstraponline <code@bootstraponline.com>
2016-05-13 15:47:17 -0400

    Fix coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,18 @@
+require 'rubygems'
 require 'coveralls'
+
+# Fix 'Outside the CI environment, not sending data.'
+# coveralls is broken: https://github.com/lemurheavy/coveralls-public/issues/782
+module Coveralls
+  def should_run?
+    true
+  end
+
+  def will_run?
+    true
+  end
+end if ENV['CI']
+
 Coveralls.wear!
 
 require_relative '../lib/xlint'
